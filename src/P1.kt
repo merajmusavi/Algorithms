cee3c1b150e01294d5bbecb790fd649e2978b694
merajuni7ed <merajmusavi1243m@gmail.com>
2023-03-19 05:19:52 +0330

    update

---
+++ src/P1.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val ArrayMeraj: Array<Int> = arrayOf(20, 40, 44, 50, 86,100)
+    val ArrayMona: Array<Int> = arrayOf(20, 40, 44, 50, 86,100)
+    var MerajWin = 0
+    var MonaWin = 0
+
+    if (ArrayMeraj.size == ArrayMona.size){
+        for (i in 0 until ArrayMeraj.size){
+            if (ArrayMeraj[i]>ArrayMona[i]){
+                MerajWin++
+            }else if (ArrayMona[i]>ArrayMeraj[i]){
+                MonaWin++
+            }
+
+
+
+        }
+        if (MerajWin>MonaWin){
+            println("winner : meraj $MerajWin")
+
+        }else if (MonaWin>MerajWin){
+            println("winner : Mona $MonaWin")
+        }else{
+            println("equal")
+        }
+    }else{
+        println("not equal")
+    }
+
+}
